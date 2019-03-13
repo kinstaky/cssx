31a8ad9552a80789abf9d2d033bdbf720a191ff9
kinstaky <1600011338@pku.edu.cn>
2019-03-14 00:33:22 +0800

    homeword 5

---
+++ likepol.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class B {
+private:
+    int nBVal;
+public:
+    void Print()
+    { cout << "nBVal="<< nBVal << endl; }
+    void Fun()
+    {cout << "B::Fun" << endl; }
+    B ( int n ) { nBVal = n;}
+};
+
+class D :public B {
+private:
+    int Dval;
+public:
+    void Print() {
+        B::Print();
+        cout << "nDVal=" << Dval << endl;
+    }
+    void Fun() {
+        cout << "D::Fun" << endl;
+    }
+    D (int n) :B(n*3) {
+        Dval = n;
+    }
+};
+
+
+int main() {
+    B * pb; D * pd;
+    D d(4); d.Fun();
+    pb = new B(2); pd = new D(8);
+    pb -> Fun(); pd->Fun();
+    pb->Print (); pd->Print ();
+    pb = & d; pb->Fun();
+    pb->Print();
+    return 0;
+}
